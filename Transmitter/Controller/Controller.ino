752f9159d91c18d5b0cb286bfc26928679b40792
jothomaspro <70159291+jothomaspro@users.noreply.github.com>
2020-08-24 23:34:36 +0200

    finishing first prototype of controller

--- Transmitter/Controller/Controller.ino
+++ Transmitter/Controller/Controller.ino
@@ -1,24 +1,30 @@
 #include "Potentiometer.h"
 #include "Joystick.h"
 #include "LoRaRadio.h"
+#include "Packet.h"
 
 Potentiometer throttle(PA3);
-Joystick aeleron(PB0, PB1, PA5, 0b0100, 0b0110);
-LoRaRadio lora(PB12, PA12, PA11);
+Joystick aeleron(PB0, PB1, PA5);
+LoRaRadio lora(PB12, PA12, PA11, 0b0100, 0b0110);
+Packet oldPacket;
+Packet currentPacket;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lora.begin(433E6);
   attachInterrupt(digitalPinToInterrupt(aeleron.getButton()), buttonIrq, CHANGE);
-  packet oldPacket;
+  
   oldPacket.motorSpeed = throttle.getPwmValue();
-  //oldPacket.aeleronR = aeleron.
+  oldPacket.aeleronR = aeleron.getPwmValueX();
+  oldPacket.aeleronL = aeleron.getPwmValueY();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  packet currentPacket;
+  currentPacket.motorSpeed = throttle.getPwmValue();
+  currentPacket.aeleronR = aeleron.getPwmValueX();
+  currentPacket.aeleronL = aeleron.getPwmValueY();
   
   if(oldPacket != currentPacket){
     lora.sendCommand(currentPacket);
